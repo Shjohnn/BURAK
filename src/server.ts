fa3afb30a8f501c16b54f7211549e79ad2d8d232
Shukurjon Nematov <shjonbek1702@gmail.com>
2026-09-23 14:49:19 +0900

    Feat:connenct via mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,14 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+import dns from "node:dns";
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+import mongosse from 'mongoose';
+mongosse.connect(process.env.MONGO_URL as string, {})
+.then((data) =>{
+    console.log("MongoDB connected succesfully!");
+    const PORT = process.env.PORT ?? 3003
+})
+.catch((err) => console.log("Error on connecting MongoDB",err))
